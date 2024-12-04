bf95ee5a3f3c5ed3ef16f811675ae3a1deaad242
wangchengcheng <sc52292@163.com>
2024-12-04 23:28:02 +0900

    sidebar已经全部搞定

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -6,25 +6,24 @@ import './index.css';
 
 // 递归渲染菜单项
 const MenuItem = ({ route, parentPath = '' }: { route: RouteItem; parentPath?: string }) => {
-    console.log('route',route);
-    
   const fullPath = parentPath ? `${parentPath}/${route.path}` : `/${route.path}`;
   
   return (
-    <li>
+    <>
       <Link to={fullPath}>{route.label}</Link>
       {route.children && (
         <ul className="submenu">
           {route.children.map(child => (
-            <MenuItem 
-              key={`${fullPath}/${child.path}`} 
-              route={child} 
-              parentPath={fullPath}
-            />
+            <li key={`${fullPath}/${child.path}`}>
+              <MenuItem 
+                route={child} 
+                parentPath={fullPath}
+              />
+            </li>
           ))}
         </ul>
       )}
-    </li>
+    </>
   );
 };
 
@@ -36,11 +35,9 @@ const Sidebar = () => {
           <nav>
             <ul>
               {menuRoutes.map(route => (
-                <div>
-                    <MenuItem key={route.path} route={route} />
-                    <h1>{route.path}</h1>
-                </div>
-
+                <li key={route.path}>
+                  <MenuItem route={route} />
+                </li>
               ))}
             </ul>
           </nav>
--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { Outlet } from 'react-router-dom';
 import ContactSon from './ContactSon/ContactSon';
 import ErrorBoundary from '../../components/ErrorBoundary';
 
@@ -12,24 +13,10 @@ const Contact: React.FC = () => {
     };
   }, []);
 
-  const [count, setCount] = useState(0);
-  const arr=[1,2,3,4,5,6,7,8,9,10]
-  const memoArr=useMemo(()=>{
-    return arr
-  },[])
-  const fn=()=>{
-    console.log('fn')
-  }
-  const memoFn=useCallback(fn,[])
   return (
     <div>
       <h1>Contact Page</h1>
-      {/* <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>增加</button>
-      <h1>儿子</h1> */}
-      {/* <ErrorBoundary fallback={<div>子组件出错了！</div>}>
-        <ContactSon arr={memoArr} fn={memoFn}/>
-      </ErrorBoundary> */}
+      <Outlet />
     </div>
   );
 };
